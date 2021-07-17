5edae21ac750abb13e2adeccc4e01d3182ff67af
LKPC <1044624802@qq.com>
2021-07-17 16:57:40 +0800

    compose

--- app/src/main/java/com/gq/composefoundation/ui/MainActivity.kt
+++ app/src/main/java/com/gq/composefoundation/ui/MainActivity.kt
@@ -2,28 +2,18 @@ package com.gq.composefoundation.ui
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Dialog
-import androidx.lifecycle.lifecycleScope
 import com.gq.basic.basis.BasicActivity
 import com.gq.basic.common.*
+import com.gq.basic.compose.LoadingDialogCompose
+import com.gq.basic.compose.rememberLoadingDialogState
 import com.gq.composefoundation.ui.theme.ComposeFoundationTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import timber.log.Timber
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : BasicActivity() {
