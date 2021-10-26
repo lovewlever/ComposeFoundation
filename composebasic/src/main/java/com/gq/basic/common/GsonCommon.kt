b150d74d9244565ebc3684df89c3730bd2eff8f0
catog <532@outlook.com>
2021-10-26 08:36:18 +0800

    Optimize Imports

--- composebasic/src/main/java/com/gq/basic/common/GsonCommon.kt
+++ composebasic/src/main/java/com/gq/basic/common/GsonCommon.kt
@@ -1,7 +1,9 @@
 package com.gq.basic.common
 
 import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 
 object GsonCommon {
     val gson by lazy { Gson() }
+    val gsonExpose by lazy { GsonBuilder().excludeFieldsWithoutExposeAnnotation().create() }
 }
