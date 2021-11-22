baad2f6f31f5811e1cb54849d364aaa247ae83b2
catog <532@outlook.com>
2021-11-22 17:14:23 +0800

    AutoHorizontalCarouselCompose

---
+++ composebasic/src/main/java/com/gq/basic/compose/AutoCarouselCompose.kt
@@ -0,0 +1,65 @@
+package com.gq.basic.compose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.pager.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+/**
+ * 自动横向轮播
+ */
+@ExperimentalPagerApi
+@Composable
+fun AutoHorizontalCarouselCompose(
+    modifier: Modifier = Modifier,
+    count: Int,
+    delay: Long = 3000,
+    pagerState: PagerState = rememberPagerState(),
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    content: @Composable PagerScope.(page: Int) -> Unit,
+) {
+
+    val coroutineScope = rememberCoroutineScope()
+    if (count > 0) {
+        DisposableEffect(key1 = Unit, effect = {
+            val job = coroutineScope.launch {
+                while (true) {
+                    delay(delay)
+                    if (count - 1 == pagerState.currentPage) {
+                        pagerState.animateScrollToPage(0)
+                    } else {
+                        pagerState.animateScrollToPage(pagerState.currentPage + 1)
+                    }
+                }
+            }
+            onDispose {
+                job.cancel()
+            }
+        })
+    }
+    Box(modifier = modifier) {
+        HorizontalPager(
+            count = count,
+            state = pagerState,
+            contentPadding = contentPadding,
+            modifier = Modifier
+                .fillMaxSize(),
+            content = content)
+        HorizontalPagerIndicator(
+            pagerState = pagerState,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp),
+        )
+    }
+}
+
