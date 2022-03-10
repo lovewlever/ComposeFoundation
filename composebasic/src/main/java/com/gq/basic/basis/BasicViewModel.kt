21198f30b480502cf5a820ae930412332900c76a
catog <532@outlook.com>
2022-03-10 14:37:44 +0800

    BasicViewModel

--- composebasic/src/main/java/com/gq/basic/basis/BasicViewModel.kt
+++ composebasic/src/main/java/com/gq/basic/basis/BasicViewModel.kt
@@ -1,11 +1,16 @@
 package com.gq.basic.basis
 
 import android.app.Application
-import androidx.lifecycle.*
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.MutableLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-open class BasicViewModel @Inject constructor(application: Application): AndroidViewModel(application), LifecycleObserver, DefaultLifecycleObserver {
+open class BasicViewModel @Inject constructor(application: Application) :
+    AndroidViewModel(application), LifecycleObserver, DefaultLifecycleObserver {
 
+        protected fun <T> getMutableLiveData() = MutableLiveData<T>()
 }
