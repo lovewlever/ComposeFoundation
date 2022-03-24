e954a03249a2962be961851e7abeded5fa6d04f8
lovewlever <lovewlever@outlook.com>
2022-03-24 16:04:10 +0800

    MediaType

---
+++ composebasic/src/main/java/com/gq/basic/extension/MediaType.kt
@@ -0,0 +1,7 @@
+package com.gq.basic.extension
+
+import okhttp3.MediaType.Companion.toMediaType
+
+val MultipartDataMediaType = "multipart/form-data".toMediaType()
+
+val ApplicationJsonMediaType = "application/json; charset=utf-8".toMediaType()
