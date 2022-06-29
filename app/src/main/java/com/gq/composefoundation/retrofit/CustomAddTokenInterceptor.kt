5f3aa2fa1ea1cd16c239cf24331f5b0c3dbd0ef8
catogq <catogq@outlook.com>
2022-06-29 11:21:40 +0800

    SdkVersion

--- app/src/main/java/com/gq/composefoundation/retrofit/CustomAddTokenInterceptor.kt
+++ app/src/main/java/com/gq/composefoundation/retrofit/CustomAddTokenInterceptor.kt
@@ -1,5 +1,6 @@
 package com.gq.composefoundation.retrofit
 
+import com.gq.basic.common.DataStoreCommon
 import okhttp3.*
 import timber.log.Timber
 
@@ -12,15 +13,18 @@ class CustomAddTokenInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request: Request = chain.request()
         val newBuilder = request.newBuilder()
-        /*val token = DataStoreCommon.getBasicTypeBySP(Constants.DataStoreKey_Token_BySp, "")
-        val userId = DataStoreCommon.getBasicTypeBySP(Constants.DataStoreKey_UserId_BySp, 0)*/
+        val token = DataStoreCommon.getBasicTypeBySP("Constants.DSKey.SPToken", "")
+        val userId = DataStoreCommon.getBasicTypeBySP("Constants.DSKey.SPUserId", -1)
         if (request.method == "GET") {
             val httpUrl: HttpUrl = request.url
                 .newBuilder()
-                .addQueryParameter("token", "token")
-                .addQueryParameter("user_id", "userId.toString()")
+                .addQueryParameter("token", token)
+                .addQueryParameter("userId", userId.toString())
                 .build()
-            newBuilder.url(httpUrl)
+            newBuilder
+                .header("token", token)
+                .header("userId", userId.toString())
+                .url(httpUrl)
         } else if (request.method == "POST") {
             val bodyBuilder = FormBody.Builder()
             if (request.body is FormBody) {
@@ -30,13 +34,16 @@ class CustomAddTokenInterceptor : Interceptor {
                     bodyBuilder.addEncoded(formBody.encodedName(i), formBody.encodedValue(i))
                 }
             }
-            bodyBuilder.addEncoded("token", "token")
-            bodyBuilder.addEncoded("user_id", "userId.toString()")
+            bodyBuilder.addEncoded("token", token)
+            bodyBuilder.addEncoded("userId", userId.toString())
             val newBody = bodyBuilder.build()
 
             for (i in 0 until newBody.size) {
                 Timber.i("${newBody.name(i)}  ${newBody.value(i)}")
             }
+            newBuilder
+                .header("token", token)
+                .header("userId", userId.toString())
             newBuilder.post(newBody)
         }
 
