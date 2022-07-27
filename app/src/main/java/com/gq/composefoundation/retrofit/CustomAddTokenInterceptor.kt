28ba0f869fffa8525609aced555f25cb8b7209d6
catogq <catogq@outlook.com>
2022-07-27 17:18:00 +0800

    Update CustomAddTokenInterceptor

--- app/src/main/java/com/gq/composefoundation/retrofit/CustomAddTokenInterceptor.kt
+++ app/src/main/java/com/gq/composefoundation/retrofit/CustomAddTokenInterceptor.kt
@@ -1,6 +1,10 @@
 package com.gq.composefoundation.retrofit
 
 import com.gq.basic.common.DataStoreCommon
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import okhttp3.*
 import timber.log.Timber
 
@@ -10,40 +14,52 @@ import timber.log.Timber
  */
 class CustomAddTokenInterceptor : Interceptor {
 
+    private val coroutineScope by lazy { CoroutineScope(Dispatchers.IO + Job()) }
+    private var token = ""
+    private var sessionId = ""
+
+    init {
+        coroutineScope.launch {
+            /*DataStoreCommon.getBasicType(Constants.DSKey.Token, "") { token ->
+                this@CustomAddTokenInterceptor.token = token
+            }*/
+        }
+        coroutineScope.launch {
+            /*DataStoreCommon.getBasicType(Constants.DSKey.SessionId, "") { sessionId ->
+                this@CustomAddTokenInterceptor.sessionId = sessionId
+            }*/
+        }
+    }
+
     override fun intercept(chain: Interceptor.Chain): Response {
         val request: Request = chain.request()
         val newBuilder = request.newBuilder()
-        val token = DataStoreCommon.getBasicTypeBySP("Constants.DSKey.SPToken", "")
-        val userId = DataStoreCommon.getBasicTypeBySP("Constants.DSKey.SPUserId", -1)
+        newBuilder
+            .header("token", token)
+            .header("sessionid", sessionId)
+
         if (request.method == "GET") {
             val httpUrl: HttpUrl = request.url
                 .newBuilder()
                 .addQueryParameter("token", token)
-                .addQueryParameter("userId", userId.toString())
+                .addQueryParameter("sessionid", sessionId)
                 .build()
-            newBuilder
-                .header("token", token)
-                .header("userId", userId.toString())
-                .url(httpUrl)
+            newBuilder.url(httpUrl)
         } else if (request.method == "POST") {
             val bodyBuilder = FormBody.Builder()
             if (request.body is FormBody) {
                 val formBody = request.body as FormBody
-
                 for (i in 0 until formBody.size) {
                     bodyBuilder.addEncoded(formBody.encodedName(i), formBody.encodedValue(i))
                 }
             }
             bodyBuilder.addEncoded("token", token)
-            bodyBuilder.addEncoded("userId", userId.toString())
+            bodyBuilder.addEncoded("sessionid", sessionId)
             val newBody = bodyBuilder.build()
 
             for (i in 0 until newBody.size) {
                 Timber.i("${newBody.name(i)}  ${newBody.value(i)}")
             }
-            newBuilder
-                .header("token", token)
-                .header("userId", userId.toString())
             newBuilder.post(newBody)
         }
 
