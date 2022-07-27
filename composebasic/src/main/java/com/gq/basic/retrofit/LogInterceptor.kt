c61aa10c9e5b5a90467dfc874bb73d5a4b5f92e8
catogq <catogq@outlook.com>
2022-07-27 17:04:18 +0800

    LogInterceptor

---
+++ composebasic/src/main/java/com/gq/basic/retrofit/LogInterceptor.kt
@@ -0,0 +1,94 @@
+package com.lk.reading.english.retrofit
+
+import com.gq.basic.common.GsonCommon
+import okhttp3.*
+import okhttp3.ResponseBody.Companion.toResponseBody
+import timber.log.Timber
+import java.io.IOException
+import java.nio.charset.Charset
+import kotlin.text.Charsets.UTF_8
+
+/**
+ * 日志蓝机器
+ */
+class LogInterceptor : Interceptor {
+
+    private val sb by lazy { StringBuilder() }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request: Request = chain.request()
+        printRequestMessage(request)
+        val response: Response = chain.proceed(request)
+        if (!response.isSuccessful) {
+            val code = response.code
+            val requestBody = response.body
+            val contentType = requestBody?.contentType()
+            val str = requestBody?.string()
+            Timber.e("<-- HTTP: ${response.code} \n${str}")
+            return response.newBuilder().body(str?.toResponseBody(contentType)).code(code).build()
+        }
+        printResponseMessage(response)
+        return response
+    }
+
+    /**
+     * 打印请求消息
+     *
+     * @param request 请求的对象
+     */
+    private fun printRequestMessage(request: Request) {
+        sb.clear()
+        val requestBody: RequestBody? = request.body
+        val headers = request.headers
+        Timber.e("--> URL: ${request.url.toUrl()}")
+        Timber.e("--> Method: ${request.method} (${requestBody?.contentLength()}-byte body)")
+        headers.toList().forEach { h -> sb.append("\n").append(h.first).append(": ").append(h.second) }
+        Timber.e("--> Headers: $sb")
+        requestBody?.contentType()?.let {
+            if (headers["Content-Type"] == null) {
+                Timber.e("--> Content-Type: $it")
+            }
+        }
+        if (requestBody?.contentLength() != -1L) {
+            if (headers["Content-Length"] == null) {
+                Timber.e("--> Content-Length: ${requestBody?.contentLength()}")
+            }
+        }
+
+        try {
+            val bufferedSink = okio.Buffer()
+            requestBody?.writeTo(bufferedSink)
+            var charset: Charset? = requestBody?.contentType()?.charset()
+            charset = charset ?: Charset.forName("utf-8")
+            Timber.e("--> Params: " + charset?.let { bufferedSink.readString(it) })
+        } catch (e: IOException) {
+            Timber.e("--> Exception: ${e.message}")
+            Timber.e(e)
+        }
+    }
+
+    /**
+     * 打印返回消息
+     *
+     * @param response 返回的对象
+     */
+    private fun printResponseMessage(response: Response) {
+        val responseBody: ResponseBody? = response.body
+        val source = responseBody?.source()
+        try {
+            source?.request(Long.MAX_VALUE) // Buffer the entire body.
+        } catch (e: IOException) {
+            Timber.e("<-- Exception: ${e.message}")
+            Timber.e(e)
+        }
+        val buffer: okio.Buffer? = source?.buffer()
+        val contentType: MediaType? = responseBody?.contentType()
+        val charset = contentType?.charset() ?: UTF_8
+        Timber.e("<-- Content-Type: $contentType")
+
+        val result: String? = buffer?.clone()?.readString(charset)
+        Timber.e("<-- Result: ${GsonCommon.gson.toJson(result)}")
+        Timber.e("<-- HTTP: ${response.code}")
+        Timber.e("<-- END: (${buffer?.size}-byte body)\n")
+    }
+}
