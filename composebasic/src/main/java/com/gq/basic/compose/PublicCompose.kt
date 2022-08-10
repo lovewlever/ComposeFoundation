febaf4d26a51cc82e4dd5152af05ed933ad18635
lovew <lovewlever@outlook.com>
2022-08-10 21:13:47 +0800

    Update
    Signed-off-by: lovew <lovewlever@outlook.com>

--- composebasic/src/main/java/com/gq/basic/compose/PublicCompose.kt
+++ composebasic/src/main/java/com/gq/basic/compose/PublicCompose.kt
@@ -1,15 +1,20 @@
 package com.gq.basic.compose
 
+
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.gq.basic.R
 
 /**
@@ -19,6 +24,10 @@ import com.gq.basic.R
 fun DialogBottomDoubleButton(
     refuseText: String = stringResource(id = R.string.cb_do_not_use),
     doneText: String = stringResource(id = R.string.cb_agree),
+    refuseTextColor: Color = MaterialTheme.colors.onPrimary,
+    doneTextColor: Color = MaterialTheme.colors.surface,
+    refuseTextSize: TextUnit = 14.sp,
+    doneTextSize: TextUnit = 14.sp,
     doneClick: () -> Unit = {},
     refuseClick: () -> Unit = {},
 ) {
@@ -35,7 +44,7 @@ fun DialogBottomDoubleButton(
                 refuseClick()
             }
         ) {
-            Text(text = refuseText)
+            Text(text = refuseText, color = refuseTextColor, fontSize = refuseTextSize)
         }
         TextButton(
             modifier = Modifier
@@ -45,7 +54,50 @@ fun DialogBottomDoubleButton(
                 doneClick()
             }
         ) {
-            Text(text = doneText)
+            Text(text = doneText, color = doneTextColor, fontSize = doneTextSize)
+        }
+    }
+}
+
+
+/**
+ * 弹窗下面的双按钮
+ */
+@Composable
+fun DialogBottomDoubleButton2(
+    cancelText: String = stringResource(id = R.string.cb_do_not_use),
+    confirmText: String = stringResource(id = R.string.cb_agree),
+    cancelTextColor: Color = MaterialTheme.colors.onPrimary,
+    confirmTextColor: Color = MaterialTheme.colors.surface,
+    cancelTextSize: TextUnit = 14.sp,
+    confirmTextSize: TextUnit = 14.sp,
+    onCancelClick: () -> Unit = {},
+    onConfirmClick: () -> Unit = {},
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(45.dp)
+    ) {
+        TextButton(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            onClick = {
+                onCancelClick()
+            }
+        ) {
+            Text(text = cancelText, color = cancelTextColor, fontSize = cancelTextSize)
+        }
+        TextButton(
+            modifier = Modifier
+                .fillMaxHeight()
+                .weight(1f),
+            onClick = {
+                onConfirmClick()
+            }
+        ) {
+            Text(text = confirmText, color = confirmTextColor, fontSize = confirmTextSize)
         }
     }
 }
