64619772ee72bfba4023667d05252f3cd744ee06
catogq <catogq@outlook.com>
2022-10-18 17:10:32 +0800

    update graph

--- app/src/main/java/com/gq/composefoundation/ui/graph/NavGraph.kt
+++ app/src/main/java/com/gq/composefoundation/ui/graph/NavGraph.kt
@@ -18,16 +18,16 @@ sealed class ScreenRoute(val route: String, val title: String) {
  * 指定当您navigate()到达此目的地时运行的动画。
  */
 fun enterTransition(): EnterTransition {
-    return slideIn(animationSpec = spring(stiffness = 200F), initialOffset = {
+    return slideIn(animationSpec = spring(stiffness = 150F), initialOffset = {
         IntOffset(x = 200, y = 0)
-    }) + fadeIn(animationSpec = tween(600))
+    }) + fadeIn(animationSpec = tween(500))
 }
 
 /**
  * 指定此目的地在经过 . 后重新进入屏幕时运行的动画popBackStack()。这默认为enterTransition
  */
 fun popEnterTransition(): EnterTransition {
-    return slideIn(animationSpec = spring(stiffness = 50F), initialOffset = {
+    return slideIn(animationSpec = spring(stiffness = 150F), initialOffset = {
         IntOffset(x = -200, y = 0)
     }) + fadeIn(animationSpec = tween(500))
 }
@@ -36,7 +36,7 @@ fun popEnterTransition(): EnterTransition {
  * 指定当您通过导航到另一个目的地离开此目的地时运行的动画。
  */
 fun exitTransition(): ExitTransition {
-    return slideOut(animationSpec = spring(stiffness = 50F), targetOffset = {
+    return slideOut(animationSpec = spring(stiffness = 150F), targetOffset = {
         IntOffset(x = -200, y = 0)
     }) + fadeOut(targetAlpha = 0.5F, animationSpec = tween(500))
 }
@@ -45,7 +45,7 @@ fun exitTransition(): ExitTransition {
  * 指定在您将其从返回堆栈中弹出后，当此目标离开屏幕时运行的动画。这默认为exitTransition.
  */
 fun popExitTransition(): ExitTransition {
-    return slideOut(animationSpec = spring(stiffness = 50F), targetOffset = {
+    return slideOut(animationSpec = spring(stiffness = 150F), targetOffset = {
         IntOffset(x = 200, y = 0)
-    }) + fadeOut(animationSpec = tween(300))
+    }) + fadeOut(animationSpec = tween(500))
 }
